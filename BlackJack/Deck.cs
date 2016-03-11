a86ebe2a29a7a18167dc9d7ef2086264676b8c61
LeonardoTouma <mr_milan1@hotmail.com>
2016-03-11 14:31:47 +0100

    Gjorde så självate switchen till kortens value

--- BlackJack/Deck.cs
+++ BlackJack/Deck.cs
@@ -9,8 +9,7 @@ namespace BlackJack
     class Deck // metod cards in deck
     {
         //List<Card> deck = new List<Card>();
-        string[] faces = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
-        string[] suits = { "♥", "♠", "♦", "♣" };
+       
 
         private Card[] deck;
         private int currentCard;
@@ -20,6 +19,8 @@ namespace BlackJack
 
         public Deck()
         {
+            List<Card> faces = new List<Card>();
+            string[] suits = { "♥", "♠", "♦", "♣" };
             deck = new Card[numberOfCards];
             currentCard = 0;
             randomNumber = new Random();
@@ -42,9 +43,62 @@ namespace BlackJack
 
             return deck[currentCard++];
         }
-        public void CardValue()
+        public void CalculateHand()
         {
-            
+            string hej = "";
+            int aces = 0;
+            int Total = 0;
+            switch(hej)
+            {
+             
+                case "King":
+                case "Queen":
+                case "Jack":
+                case "10":
+                    Total += 10;
+                         break;
+                case
+                    "9":
+                    Total += 9;
+                    break;
+
+
+                case
+                    "8":
+                    Total += 8;
+                    break;
+                case
+                    "7":
+                    Total += 7;
+                    break;
+                case
+                    "6":
+                    Total += 6;
+                    break;
+                case
+                    "5":
+                    Total += 5;
+                    break;
+                case
+                    "4":
+                    Total += 4;
+                    break;
+                case
+                    "3":
+                    Total += 3;
+                    break;
+                case "2":
+                    Total += 2;
+                    break;
+
+                default:
+                    throw new Exception("Didnt recognize your card");
+                    break;
+                    
+
+
+
+            }
         }
 
 
